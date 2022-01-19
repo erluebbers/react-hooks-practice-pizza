a6e0d5adb030d7bf4e3c4b22972c7ddd9f63dedf
Erik Luebbers <erluebbers@gmail.com>
2022-01-19 05:18:43 -0800

    commit tree 71e9729a00a633cdd6cf48a6b5246c6115285868

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,10 +1,45 @@
 import React from "react";
 
-function PizzaForm( {editPizzaObj, setEditPizzaObj, handleSubmit} ) {
+function PizzaForm( {editPizzaObj, setEditPizzaObj, setPizzas} ) {
   const { id, topping, size, vegetarian } = editPizzaObj
 
+  function handleToppingEdit(event) {
+    setEditPizzaObj({
+      ...editPizzaObj,
+      topping: event.target.value
+    })
+  }
+
+  function handleSizeEdit(event) {
+    setEditPizzaObj({
+      ...editPizzaObj,
+      size: event.target.value
+    })
+  }
+
+  function handleVegetarianEdit(event) {
+    const veg = event.target.value === "Vegetarian" ? true : false
+    debugger
+    setEditPizzaObj({
+      ...editPizzaObj,
+      vegetarian: veg
+    })
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    fetch(`http://localhost:3001/pizzas/${id}`, {
+      method: "PATCH",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(editPizzaObj)
+    })
+    fetch("http://localhost:3001/pizzas")
+      .then(r => r.json())
+      .then(data => setPizzas(data))
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={(e) => handleSubmit(e)}>
       <div className="form-row">
         <div className="col-5">
           <input
@@ -13,10 +48,11 @@ function PizzaForm( {editPizzaObj, setEditPizzaObj, handleSubmit} ) {
             name="topping"
             placeholder="Pizza Topping"
             value={topping}
+            onChange={handleToppingEdit}
           />
         </div>
         <div className="col">
-          <select className="form-control" name="size" value={size}>
+          <select className="form-control" name="size" value={size} onChange={handleSizeEdit}>
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -30,6 +66,7 @@ function PizzaForm( {editPizzaObj, setEditPizzaObj, handleSubmit} ) {
               name="vegetarian"
               value="Vegetarian"
               checked={vegetarian}
+              onChange={handleVegetarianEdit}
             />
             <label className="form-check-label">Vegetarian</label>
           </div>
@@ -40,6 +77,7 @@ function PizzaForm( {editPizzaObj, setEditPizzaObj, handleSubmit} ) {
               name="vegetarian"
               value="Not Vegetarian"
               checked={!vegetarian}
+              onChange={handleVegetarianEdit}
             />
             <label className="form-check-label">Not Vegetarian</label>
           </div>
